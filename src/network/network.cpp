91610360e2569f7ab2288cc064c7b418709c5965
Osvaldo Pereira Barzaga <opereirab@gmail.com>
2021-05-20 10:10:28 +0300

    *

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -18,164 +18,164 @@ void Network::onEvent(arduino_event_id_t event, arduino_event_info_t info)
     case ARDUINO_EVENT_WIFI_READY: {
       Logger::log("ARDUINO_EVENT_WIFI_READY");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_SCAN_DONE: {
       Logger::log("ARDUINO_EVENT_WIFI_SCAN_DONE");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_START: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_START");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_STOP: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_STOP");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_CONNECTED: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_CONNECTED");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_DISCONNECTED: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_DISCONNECTED");
       WiFi.disconnect();
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_GOT_IP: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_GOT_IP");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_GOT_IP6: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_GOT_IP6");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_STA_LOST_IP: {
       Logger::log("ARDUINO_EVENT_WIFI_STA_LOST_IP");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_AP_START: {
       Logger::log("ARDUINO_EVENT_WIFI_AP_START");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_AP_STOP: {
       Logger::log("ARDUINO_EVENT_WIFI_AP_STOP");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_AP_STACONNECTED: {
       Logger::log("ARDUINO_EVENT_WIFI_AP_STACONNECTED");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED: {
       Logger::log("ARDUINO_EVENT_WIFI_AP_STADISCONNECTED");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED: {
       Logger::log("ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_AP_PROBEREQRECVED: {
       Logger::log("ARDUINO_EVENT_WIFI_AP_PROBEREQRECVED");
       break;
-    },
+    }
     case ARDUINO_EVENT_WIFI_AP_GOT_IP6: {
       Logger::log("ARDUINO_EVENT_WIFI_AP_GOT_IP6");
       break;
-    },
+    }
     case ARDUINO_EVENT_ETH_START: {
       Logger::log("ARDUINO_EVENT_ETH_START");
       break;
-    },
+    }
     case ARDUINO_EVENT_ETH_STOP: {
       Logger::log("ARDUINO_EVENT_ETH_STOP");
       break;
-    },
+    }
     case ARDUINO_EVENT_ETH_CONNECTED: {
       Logger::log("ARDUINO_EVENT_ETH_CONNECTED");
       break;
-    },
+    }
     case ARDUINO_EVENT_ETH_DISCONNECTED: {
       Logger::log("ARDUINO_EVENT_ETH_DISCONNECTED");
       break;
-    },
+    }
     case ARDUINO_EVENT_ETH_GOT_IP: {
       Logger::log("ARDUINO_EVENT_ETH_GOT_IP");
       break;
-    },
+    }
     case ARDUINO_EVENT_ETH_GOT_IP6: {
       Logger::log("ARDUINO_EVENT_ETH_GOT_IP6");
       break;
-    },
+    }
     case ARDUINO_EVENT_WPS_ER_SUCCESS: {
       Logger::log("ARDUINO_EVENT_WPS_ER_SUCCESS");
       break;
-    },
+    }
     case ARDUINO_EVENT_WPS_ER_FAILED: {
       Logger::log("ARDUINO_EVENT_WPS_ER_FAILED");
       break;
-    },
+    }
     case ARDUINO_EVENT_WPS_ER_TIMEOUT: {
       Logger::log("ARDUINO_EVENT_WPS_ER_TIMEOUT");
       break;
-    },
+    }
     case ARDUINO_EVENT_WPS_ER_PIN: {
       Logger::log("ARDUINO_EVENT_WPS_ER_PIN");
       break;
-    },
+    }
     case ARDUINO_EVENT_WPS_ER_PBC_OVERLAP: {
       Logger::log("ARDUINO_EVENT_WPS_ER_PBC_OVERLAP");
       break;
-    },
+    }
     case ARDUINO_EVENT_SC_SCAN_DONE: {
       Logger::log("ARDUINO_EVENT_SC_SCAN_DONE");
       break;
-    },
+    }
     case ARDUINO_EVENT_SC_FOUND_CHANNEL: {
       Logger::log("ARDUINO_EVENT_SC_FOUND_CHANNEL");
       break;
-    },
+    }
     case ARDUINO_EVENT_SC_GOT_SSID_PSWD: {
       Logger::log("ARDUINO_EVENT_SC_GOT_SSID_PSWD");
       break;
-    },
+    }
     case ARDUINO_EVENT_SC_SEND_ACK_DONE: {
       Logger::log("ARDUINO_EVENT_SC_SEND_ACK_DONE");
       break;
-    },
+    }
     case ARDUINO_EVENT_PROV_INIT: {
       Logger::log("ARDUINO_EVENT_PROV_INIT");
       break;
-    },
+    }
     case ARDUINO_EVENT_PROV_DEINIT: {
       Logger::log("ARDUINO_EVENT_PROV_DEINIT");
       break;
-    },
+    }
     case ARDUINO_EVENT_PROV_START: {
       Logger::log("ARDUINO_EVENT_PROV_START");
       break;
-    },
+    }
     case ARDUINO_EVENT_PROV_END: {
       Logger::log("ARDUINO_EVENT_PROV_END");
       break;
-    },
+    }
     case ARDUINO_EVENT_PROV_CRED_RECV: {
       Logger::log("ARDUINO_EVENT_PROV_CRED_RECV");
       break;
-    },
+    }
     case ARDUINO_EVENT_PROV_CRED_FAIL: {
       Logger::log("ARDUINO_EVENT_PROV_CRED_FAIL");
       break;
-    },
+    }
     case ARDUINO_EVENT_PROV_CRED_SUCCESS: {
       Logger::log("ARDUINO_EVENT_PROV_CRED_SUCCESS");
       break;
-    },
+    }
     case ARDUINO_EVENT_MAX: {
       Logger::log("ARDUINO_EVENT_MAX");
       break;
-    },
+    }
     default: {
       Logger::log("ARDUINO_EVENT_UNDEFINED");
     }  
